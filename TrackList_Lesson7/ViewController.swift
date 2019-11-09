c33ee3e6cc03ba25cdb731b2ac15fcf0b4d89899
Artjoms Vorona <artjomsvorona@gmail.com>
2019-11-09 10:05:59 +0200

    Initial Commit

---
+++ TrackList_Lesson7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackList_Lesson7
+//
+//  Created by Artjoms Vorona on 09/11/2019.
+//  Copyright © 2019 Artjoms Vorona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
